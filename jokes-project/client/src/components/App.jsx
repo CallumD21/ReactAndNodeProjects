4ab82ac6fdb7c3958cef05a104df8256de30c589
CallumD21 <callum.dickinson176@gmail.com>
2023-11-11 22:07:28 +0000

    Added api call to frontend to get a random joke

--- jokes-project/client/src/components/App.jsx
+++ jokes-project/client/src/components/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+    const [joke, setJoke] = useState(null);
+    useEffect(() => {
+        fetch("/joke/random")
+            .then((res) => res.json())
+            .then((data) => setJoke(data));
+    }, []);
+
     return (
         <div>
             <h1>Jokes Project</h1>
+            <p>{!joke ? "Loading..." : joke.jokeText}</p>
         </div>
     )
 }

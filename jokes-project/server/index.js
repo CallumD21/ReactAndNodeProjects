51edcd600dc87f682f82c816d7f6caf33b13347c
CallumD21 <callum.dickinson176@gmail.com>
2023-11-11 19:50:04 +0000

    Added api call for getting a random joke

--- jokes-project/server/index.js
+++ jokes-project/server/index.js
@@ -4,6 +4,11 @@ import jokes from "./jokes.js";
 const app = express();
 const port = 3001;
 
+app.get("/joke/random", (req, res) => {
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+    res.json(jokes[randomIndex]);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });

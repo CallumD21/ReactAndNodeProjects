1868417e3fc8e33b0d844115326e15232d1b86ca
CallumD21 <callum.dickinson176@gmail.com>
2023-11-12 17:34:22 +0000

    Added api call for getting all jokes and paginated it - 10 jokes per page

--- jokes-project/server/index.js
+++ jokes-project/server/index.js
@@ -9,6 +9,13 @@ app.get("/joke/random", (req, res) => {
     res.json(jokes[randomIndex]);
 });
 
+app.get("/joke/all?", (req, res) => {
+    const pageIndex = parseInt(req.query.page);
+    const startIndex = (pageIndex - 1) * 10;
+    var jokePage = jokes.slice(startIndex, startIndex + 10);
+    res.json(jokePage);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
